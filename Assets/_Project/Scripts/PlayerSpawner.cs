68352917a6442f3ce3d487ebae630cc8b232f254
MasaMalkawi <masamalkawi@gmail.com>
2025-06-02 14:48:16 +0300

    solve 104 error

--- Assets/_Project/Scripts/PlayerSpawner.cs
+++ Assets/_Project/Scripts/PlayerSpawner.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using Photon.Pun;
 
 public class PlayerSpawner : MonoBehaviourPunCallbacks
@@ -11,3 +11,22 @@ public class PlayerSpawner : MonoBehaviourPunCallbacks
         PhotonNetwork.Instantiate(Player.name, spawnPos, Quaternion.identity);
     }
 }
+/*
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject Player;
+
+    void Start()
+    {
+        
+        if (!PhotonNetwork.InRoom)
+            return;
+
+        Vector3 spawnPos = new Vector3(Random.Range(-5f, 5f), 1f, Random.Range(-5f, 5f));
+        PhotonNetwork.Instantiate(Player.name, spawnPos, Quaternion.identity);
+    }
+}
+*/
