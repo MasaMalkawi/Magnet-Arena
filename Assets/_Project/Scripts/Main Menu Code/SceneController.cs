938027580094c80da2cd460912fbd5344c9a38e5
MasaMalkawi <masamalkawi@gmail.com>
2025-06-04 14:34:34 +0300

    Scene Controller

--- Assets/_Project/Scripts/Main Menu Code/SceneController.cs
+++ Assets/_Project/Scripts/Main Menu Code/SceneController.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 using Photon.Pun; 
 
 public class SceneController : MonoBehaviour
-{
+{/*
     public void LoadScene(string SceneName)
     {
         PhotonNetwork.LoadLevel("GameScene"); 
     }
-
+*/
     public void QuitGame()
     {
         Application.Quit();
